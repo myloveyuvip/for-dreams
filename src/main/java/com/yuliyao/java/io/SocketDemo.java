416ed3f3d5b95977af83825834f1c335c8c462c1
yuliyao <myloveyuvip@qq.com>
2020-07-09 22:49:44 +0800

    java io socket

--- src/main/java/com/yuliyao/java/io/SocketDemo.java
+++ src/main/java/com/yuliyao/java/io/SocketDemo.java
@@ -1,10 +1,14 @@
 package com.yuliyao.java.io;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 /**
  * @author YuLiyao
@@ -14,11 +18,31 @@ public class SocketDemo {
 
     public void socketServer() throws IOException {
         ServerSocket serverSocket = new ServerSocket(6666);
-        Socket client = serverSocket.accept();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
+        boolean isRun = true;
+        while (isRun) {
+            Socket client = serverSocket.accept();
+            System.out.println("一个客户端连接！");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+            String line;
+            while (!"bye".equals(line = bufferedReader.readLine())) {
+                System.out.println("client：" + line);
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("server:");
+                String returnMsg = scanner.nextLine();
+                bufferedWriter.write(returnMsg + "\r");
+                bufferedWriter.flush();
+            }
+            bufferedWriter.close();
+            isRun = false;
 
         }
+        System.out.println("连接结束！");
+    }
+
+    public static void main(String[] args) throws IOException {
+        SocketDemo socketDemo = new SocketDemo();
+        socketDemo.socketServer();
     }
 }
--- src/test/java/com/yuliyao/java/io/SocketDemoTest.java
+++ src/test/java/com/yuliyao/java/io/SocketDemoTest.java
@@ -2,10 +2,13 @@ package com.yuliyao.java.io;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
-
-import static org.junit.Assert.*;
+import java.util.Scanner;
 
 /**
  * @author YuLiyao
@@ -13,10 +16,65 @@ import static org.junit.Assert.*;
  */
 public class SocketDemoTest {
 
+    private SocketDemo socketDemo = new SocketDemo();
+
+
     @Test
     public void testConnectSocket() throws IOException {
         Socket socket = new Socket("localhost", 6666);
+        boolean isRun = true;
+        while (isRun) {
+
+            System.out.println("client:");
+            Scanner scanner = new Scanner(System.in);
+            String clinetMsg = scanner.nextLine();
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            bufferedWriter.write(clinetMsg + "\r");
+            bufferedWriter.flush();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String line;
+            while (!(line = reader.readLine()).equals("bye")) {
+
+                System.out.println("server:" + line);
+                Scanner scanner1 = new Scanner(System.in);
+                System.out.println("client:");
+                String msg1 = scanner1.nextLine();
+                bufferedWriter.write(msg1 + "\r");
+                bufferedWriter.flush();
+            }
+            bufferedWriter.close();
+            scanner.close();
+            isRun = false;
+        }
+
+
 
     }
+
+    @Test
+    public void socketServer() throws IOException {
+        socketDemo.socketServer();
+
+    }
+
+    @Test
+    public void testSystemIn() {
+       Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入：");
+        String s = scanner.nextLine();
+        System.out.println("您输入了：" + s);
+
+    }
+
+    @Test
+    public void testString() {
+        String str = "test";
+        String st1 = "test";
+        System.out.println(str == st1);
+        System.out.println(str == "test");
+    }
+
+
 
 }
